5db0fb572930238a86e49329b489733150a12538
Jerry Velazquez <jvr572@gmail.com>
2019-07-05 14:51:15 -0400

    "controlled form done"

--- budget_buddy_fe/src/components/itemForm.js
+++ budget_buddy_fe/src/components/itemForm.js
@@ -8,20 +8,27 @@ export default class ItemForm extends React.Component {
     itemName: "",
     targetGoal: 0
   }
+
+  handleChange = (evt) => {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
   render() {
+    const {imgURL, itemName, targetGoal} = this.state
     return(
       <form className="ui form">
         <div class="field">
           <label>Image URL</label>
-          <input type="url"/>
+          <input onChange={this.handleChange} name="imgURL"  value={imgURL} type="url" placeholder="https://www.wikihow.com/aid2627945-v4-900px-2627945-1-1.jpg"/>
         </div>
         <div class="field">
           <label>Item Name</label>
-          <input type="text"/>
+          <input onChange={this.handleChange} name="itemName" value={itemName} type="text" placeholder="Boosted Board"/>
         </div>
         <div class="field">
           <label>Price</label>
-          <input type="number"/>
+          <input onChange={this.handleChange} name="targetGoal" value={targetGoal} type="number" placeholder="$$$"/>
         </div>
           <input className="ui button" type="submit"/>
       </form>
