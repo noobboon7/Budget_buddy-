64a563b48630ca342c203ebc4c05d54d459d3965
Jerry Velazquez <jvr572@gmail.com>
2019-07-05 14:07:31 -0400

    "added buttons to items

--- budget_buddy_fe/src/container/SideMenu.js
+++ budget_buddy_fe/src/container/SideMenu.js
@@ -5,14 +5,17 @@ class SideMenu extends Component {
     itemList: []
   }
   handleClick = (e) => {
-    console.log("clicked add button");
-
+    // console.log("clicked add button");
     this.setState({
       itemList: [...this.state.itemList,
         <div className= "item">
           <li >BUDGET ITEM</li>
         </div>]
     })
+  }
+
+  itemButton = (evt) => {
+    console.log("item bnt clicked");
   }
 
 
@@ -26,12 +29,24 @@ class SideMenu extends Component {
         <ul>
         {this.state.itemList.map(item => {
           return(
-            <li>item</li>
+            <li>
+              <img src="" alt="img here" />
+              <h4>item name</h4>
+              <h4>target goal</h4>
+              <button onClick={this.itemButton}
+                      name="edit"
+                      className="ui blue button">
+                Edit
+              </button>
+              <button onClick={this.itemButton}
+                      name="delete"
+                      className="ui red button">
+                Delete
+              </button>
+
+            </li>
           )
         })}
-        <div className= "item">
-          <li className= "item">BUDGET ITEM</li>
-        </div>
         </ul>
       </div>
     )
