eef202edf27f6e2a2b0660489dd997f6381c22e6
Jerry Velazquez <jvr572@gmail.com>
2019-07-11 10:11:05 -0400

    "fonts on register page"

--- budget_buddy_fe/src/components/Register.js
+++ budget_buddy_fe/src/components/Register.js
@@ -34,52 +34,52 @@ class Register extends Component {
     const {username, password, email, first_name, last_name,bio} = this.state
     return (
       <>
-      <div id="registerform" className="RegisterPage">
+      <div className="RegisterPage">
       <CarouselReg className="Carousel"/>
-        <div id="registerform" className = "ui container ">
-          <form id="registerform" className="ui form" onSubmit={this.handleSubmit}>
+        <div className = "ui container ">
+          <form className="ui form" onSubmit={this.handleSubmit}>
           <br/>
-          <h4 className="ui dividing header">Register</h4>
+          <h4 id="registerform" className="ui dividing header">Register</h4>
 
-          <div className=" two fields">
-            <div id="registerform" className="field">
-              <label>First Name:</label>
+          <div id="registerform" className=" two fields">
+            <div  className="field">
+              <label id="registerform">First Name:</label>
               <div className="ui left icon input">
                 <input type="text" name="first_name" value={first_name} onChange={this.handleChange}/>
                 <i className="keyboard outline icon"></i>
               </div>
             </div>
             <div id="registerform" className="field">
-              <label>Last Name:</label>
+              <label id="registerform">Last Name:</label>
               <div className="ui left icon input">
                 <input type="text" name="last_name" value={last_name} onChange={this.handleChange}/>
                 <i className="keyboard outline icon"></i>
               </div>
             </div>
           </div>
-            <div id="registerform" className="field">
-              <label>Email:</label>
+            <div className="field">
+              <label id="registerform">Email:</label>
               <div className="ui left icon input">
               <input type="email" name="email" value={email} onChange={this.handleChange}/>
               <i className="envelope icon"></i>
               </div>
             </div>
-            <div id="registerform" className="field">
-              <label>Username:</label>
+            <div className="field">
+              <label id="registerform">Username:</label>
               <div className="ui left icon input">
                 <input type="text" name="username" value={username} onChange={this.handleChange}/>
                 <i className="user icon"></i>
               </div>
             </div>
-            <div id="registerform" className="field">
-              <label>Password:</label>
+            <div className="field">
+              <label id="registerform">Password:</label>
               <div className="ui left icon input">
                 <input type="password" name="password" value={password} onChange={this.handleChange}/>
                 <i className="lock icon"></i>
               </div>
             </div>
-            <div id="registerform" className="field">
-              <label>Bio:</label>
+            <div  className="field">
+              <label id="registerform">Bio:</label>
               <div className="ui left icon input">
                 <input type="text" name="bio" value={bio} onChange={this.handleChange}/>
                 <i className="keyboard outline icon"></i>
